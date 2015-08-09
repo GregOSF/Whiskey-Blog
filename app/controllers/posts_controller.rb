c2273957da87dae91c6e02f9217ee7e3d41e89cf
GregOSF <gregory.onza@gmail.com>
2015-08-09 15:33:37 -0700

    Initial commit of new User and Site controllers or models

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,11 @@
+class PostsController < ApplicationController
+
+	def index
+		@posts = Post.all
+		render :index
+	end
+
+	
+
+
+end
---
+++ app/controllers/site_controller.rb
@@ -0,0 +1,5 @@
+class SiteController < ApplicationController
+	def index
+		render :site		
+	end
+end
