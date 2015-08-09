129fe12ea8932b17f3040703961f0c611b777dc1
GregOSF <gregory.onza@gmail.com>
2015-08-09 16:46:31 -0700

    Modify create and show methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,23 @@
 class UsersController < ApplicationController
   def new
+  	@user = User.new
+  	render :new
   end
 
   def create
+  	user = User.create(user_params)
+  	session[:user_id] = user.id
+  	redirect_to root_path
   end
 
   def show
+  	@user = User.find(session[:user_id])
+    render :show
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
+
 end
